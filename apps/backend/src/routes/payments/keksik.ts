ff8021e81a6be8af2d76062e0b6495b3fcc265b6
mvznfcoqe <gearboxop@gmail.com>
2025-12-17 22:16:40 +0000

    Debug keksik

--- apps/backend/src/routes/payments/keksik.ts
+++ apps/backend/src/routes/payments/keksik.ts
@@ -22,34 +22,19 @@ const keksikCallbackSchema = z.object({
 	hash: z.string(),
 });
 
-export const paymentsKeksikRoute = new Hono().post(
-	"/",
-	zValidator("form", keksikCallbackSchema, (result, ctx) => {
-		if (!result.success) {
-			logger.error(
-				`Keksik callback validation failed: ${JSON.stringify(result.error.issues)}`,
-			);
-			logger.error(`Body: ${JSON.stringify(ctx.req.json())}`);
-			logger.error(`Params: ${JSON.stringify(ctx.req.param())}`);
-			logger.error(`Query: ${JSON.stringify(ctx.req.queries())}`);
-			logger.error(`Form Data: ${JSON.stringify(ctx.req.formData())}`);
+export const paymentsKeksikRoute = new Hono().post("/", async (ctx) => {
+	console.log(ctx);
+	// const { type, data } = ctx.req.valid("form");
 
-			return ctx.status(400);
-		}
-	}),
-	async (ctx) => {
-		const { type, data } = ctx.req.valid("form");
+	// if (type === "confirmation") {
+	// 	return ctx.json({ status: "ok", code: env.KEKSIK_CONFIRMATION_CODE });
+	// }
 
-		if (type === "confirmation") {
-			return ctx.json({ status: "ok", code: env.KEKSIK_CONFIRMATION_CODE });
-		}
+	// if (type === "new_donate") {
+	// 	logger.info(`Received new donation from Keksik: ${JSON.stringify(data)}`);
 
-		if (type === "new_donate") {
-			logger.info(`Received new donation from Keksik: ${JSON.stringify(data)}`);
+	// 	return ctx.json({ status: "ok" });
+	// }
 
-			return ctx.json({ status: "ok" });
-		}
-
-		return ctx.json({ status: "ok" });
-	},
-);
+	return ctx.json({ status: "ok" });
+});
